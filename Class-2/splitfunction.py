a2b222f3291c32ae22fb59b43d3e040e61f6761d
towhidulislam99 <towhidulislam618@gmail.com>
2023-08-24 03:17:06 -0700

    Folder Added in this Course

--- Class-2/splitfunction.py
+++ Class-2/splitfunction.py
@@ -16,3 +16,11 @@ if len(insp1) == 0 or len(insp2) == 0:
     print("Data Cannot be Inserted")
 else:
     print("Data Successfully Inserted")
+
+inp1 = input("Please Enter the first and last name here: ")
+inpsp = inp1.split()
+
+if len(inpsp) < 2:
+    print("Please Enter the Last Name here, Data Cannot be Stored")
+else:
+    print("Data Successfully Stored Here")
